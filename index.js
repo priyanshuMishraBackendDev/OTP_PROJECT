50a5bbe23953d1b8ef0dd3abb3a0efe31bb236fb
priyanshuMishraBackendDev <134582250+priyanshuMishraBackendDev@users.noreply.github.com>
2023-08-17 15:58:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const {dbConnectAndSync} = require("./Utils/db")
 const app = express();
 const port = 4000;
 const server = require('http').Server(app);
-// const io = require('socket.io')(server);
+const io = require('socket.io')(server);
 
 const connectionString = 'mongodb+srv://root:hq4TbQ1NwYY3PRxF@priyanshupersonal.q9jelbo.mongodb.net/OTP_GENERATOR?retryWrites=true&w=majority';
 const dbOptions = {
@@ -13,7 +13,7 @@ const dbOptions = {
 //   useCreateIndex: true,
   };
 dbConnectAndSync(connectionString, dbOptions).then((list)=>{
-    // require('./api')(app, io)
+    require('./api')(app, io)
 
     server.listen(port, async () => {
         console.log(`Listening on port ${port}`)
